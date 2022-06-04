24fdc1f6686ef6d938f9fd7d11d3287ec22ada1e
Timofei Nazarov <timofejnazarov@gmail.com>
2022-06-05 01:09:02 +0300

    Добавлены файлы с заданиями к 4му уроку

---
+++ nazarov_timofei_dz_4/task_4_4.py
@@ -0,0 +1,6 @@
+from utils import currency_rates
+
+currency_rates('qqq')
+currency_rates('EUR')
+currency_rates('Aud')
+currency_rates('usd')
---
+++ nazarov_timofei_dz_4/task_4_5.py
@@ -0,0 +1,5 @@
+from sys import argv
+from utils import currency_rates
+
+currency_rates(argv[1])
+
